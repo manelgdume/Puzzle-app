6cf5a64262f1a4a3bf3b7b47c1b031cf0abe4d30
manelgdume <142036063+manelgdume@users.noreply.github.com>
2024-02-25 12:21:44 +0100

    hola

--- app/src/main/java/com/example/puzle/Game.java
+++ app/src/main/java/com/example/puzle/Game.java
@@ -1,10 +1,13 @@
 package com.example.puzle;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.util.Log;
 
 public class Game {
     private Bitmap[] solution;
+    private BD bd = new BD();
+    long tiempoInicial = System.currentTimeMillis();
 
     public Game(Bitmap[] solution) {
         this.solution = solution;
@@ -15,6 +18,20 @@ public class Game {
                 return false;
             }
         }
+        long tiempo_final = System.currentTimeMillis()-tiempoInicial ;
+        Log.d("TAG", String.valueOf(formatTime(tiempo_final)));
+        Result r = new Result(tiempo_final);
+        bd.saveScore(r);
         return true;
+    }
+    private void saveSolution(String time){
+
+    }
+    @SuppressLint("DefaultLocale")
+    private String formatTime(long milisegundos) {
+        long minutos = (milisegundos / 1000) / 60;
+        long segundos = (milisegundos / 1000) % 60;
+
+        return String.format("%02d:%02d", minutos, segundos);
     }
 }
--- app/src/main/java/com/example/puzle/MainActivity.java
+++ app/src/main/java/com/example/puzle/MainActivity.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BD bd = new BD();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnPlay = findViewById(R.id.btnPlay);
+        TableLayout tableLayout = findViewById(R.id.tableLayout);
+        bd.getResults(tableLayout,this);
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,4 +28,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
