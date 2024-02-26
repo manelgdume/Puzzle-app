2cc0f260611b46628f0c2eff9d8bf4c16b0d33b9
manelgdume <142036063+manelgdume@users.noreply.github.com>
2024-02-26 10:54:52 +0100

    settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,9 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.recyclerview:recyclerview:1.3.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Puzle"
+rootProject.name = "puzle"
 include(":app")
- 
